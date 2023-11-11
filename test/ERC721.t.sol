66b561288fd7241c3d61401874b8ca4e3237ac67
artchen <aechen1202@hotmail.com>
2023-11-12 01:42:53 +0800

    add test case

--- test/ERC721.t.sol
+++ test/ERC721.t.sol
@@ -16,23 +16,130 @@ contract ERC721Test is Test {
         _erc721Nft = new erc721Nft();
     }
 
-    function test_erc721A() public {
+    //案例1:mint 1 個
+	function test_erc721A_Mint1Token() public {
+        vm.startPrank(user1);
+        _erc721aNft.mint(user1, 1);
+        _erc721Nft.mint(user1, 1);
+    }
+
+    //案例2: mint 100 個
+	function test_erc721A_Mint100Token() public {
+        vm.startPrank(user1);
+        _erc721aNft.mint(user1, 100);
+        _erc721Nft.mint(user1, 100);
+    }
+
+    //案例3: mint 10000 個
+    function test_erc721A_Mint10000Token() public {
+        vm.startPrank(user1);
+        _erc721aNft.mint(user1, 10000);
+        _erc721Nft.mint(user1, 10000);
+    }
+
+    //案例4:mint 10, approve token id =0
+    function test_erc721A_approve0() public {
         vm.startPrank(user1);
         
         _erc721aNft.mint(user1, 10);
 
          _erc721aNft.approve(user2, 0);
-        
-        _erc721aNft.transferFrom(user1, user2, 2);
     }
 
-    function test_erc721() public {
+    //案例4:mint 10, approve token id =0
+    function test_erc721_approve0() public {
         vm.startPrank(user1);
 
         _erc721Nft.mint(user1, 10);
 
         _erc721Nft.approve(user2, 0);
         
-        _erc721Nft.transferFrom(user1, user2, 2);
     }
+
+    //案例5:mint 10, approve token id =9
+    function test_erc721A_approve9() public {
+        vm.startPrank(user1);
+        
+        _erc721aNft.mint(user1, 10);
+
+         _erc721aNft.approve(user2, 9);
+    }
+
+    //案例5:mint 10, approve token id =9
+    function test_erc721_approve9() public {
+        vm.startPrank(user1);
+
+        _erc721Nft.mint(user1, 10);
+
+        _erc721Nft.approve(user2, 9);
+        
+    }
+
+    //案例6: approve all
+    function test_erc721A_approveAll() public {
+        vm.startPrank(user1);
+        _erc721aNft.setApprovalForAll(user2, true);
+    }
+
+    //案例6: approve all
+    function test_erc721_approveAll() public {
+        vm.startPrank(user1);
+        _erc721Nft.setApprovalForAll(user2, true);
+        
+    }
+
+    //案例7: mint 10, transfer  token id =0
+    function test_erc721A_transfer0() public {
+        vm.startPrank(user1);
+        
+        _erc721aNft.mint(user1, 10);
+        
+        _erc721aNft.transferFrom(user1, user2, 0);
+    }
+
+    //案例7: mint 10, transfer  token id =0
+    function test_erc721_transfer0() public {
+        vm.startPrank(user1);
+
+        _erc721Nft.mint(user1, 10);
+        
+        _erc721Nft.transferFrom(user1, user2, 0);
+    }
+
+    //案例8: mint 10, transfer  token id =9
+    function test_erc721A_transfer9() public {
+        vm.startPrank(user1);
+        
+        _erc721aNft.mint(user1, 10);
+        
+        _erc721aNft.transferFrom(user1, user2, 9);
+    }
+
+    //案例8: mint 10, transfer  token id =9
+    function test_erc721_transfer9() public {
+        vm.startPrank(user1);
+
+        _erc721Nft.mint(user1, 10);
+        
+        _erc721Nft.transferFrom(user1, user2, 9);
+    }
+
+    //案例9: mint 11, transfer  token id =9
+    function test_erc721A_mint11_transfer9() public {
+        vm.startPrank(user1);
+        
+        _erc721aNft.mint(user1, 11);
+        
+        _erc721aNft.transferFrom(user1, user2, 9);
+    }
+
+    //案例9: mint 11, transfer  token id =9
+    function test_erc721_mint11_transfer9() public {
+        vm.startPrank(user1);
+
+        _erc721Nft.mint(user1, 11);
+        
+        _erc721Nft.transferFrom(user1, user2, 9);
+    }
+
 }
